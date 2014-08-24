27e15e9113af010627c05eabfd4b0c87971e6809
ryota miya <rmiya56@gmail.com>
2014-08-24 20:59:17 +0900

    building basic directories..

---
+++ fileio/jcamp.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+u"""
+    JCAMP-DX format file read
+    http://www.jcamp-dx.org/
+"""
+
+
+def read():
+    pass
+
+
+def write():
+    pass
---
+++ preprocessing/savitzky_golay.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+u"""
+    Savitzky-Golay Smoothing
+    http://wiki.scipy.org/Cookbook/SavitzkyGolay
+"""
+
+def savgol():
+
+    pass
+
+
+
+if __name__ == '__main__':
+    
+    savgol()
