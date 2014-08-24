6099d1be28369603031b0e5bf294eba521546d76
ryota miya <rmiya56@gmail.com>
2014-08-24 21:36:04 +0900

    implement snv and detrend

--- preprocessing/detrend.py
+++ preprocessing/detrend.py
@@ -1,6 +1,33 @@
 # coding: utf-8
+import numpy as np
 u"""
 """
+
+
+
+def detrend(x, poly_order):
+    u"""
+    remove a trend data points have
+    you can desinate polynominal order
+    of fitting curves(or line) for estimating the trend.
+    
+    [args]
+        x:            1d-ndarray
+        poly_order:   polynominal order of fitting curve 
+    [return]
+        trend removed x
+    """
+
+    index   = np.arange( len(x) )
+    coefs   = np.polyfit( index, x, poly_order )
+    
+    for order, coef in enumerate(reversed(coefs)):
+        for i in range(len(x)):
+            x[i] -= coef * pow( index[i], order )
+    
+    return x
+
+
 
 if __name__ == '__main__':
     pass
---
+++ preprocessing/snv.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import numpy as np
+u"""
+    SNV: normalizing
+"""
+
+def snv(x):
+    u"""
+    centering to mean value 
+    and normalized by standard deviation
+    [args]
+        x:    1d-ndarray
+    [return]
+        normalized x
+    """
+
+    mean        = np.mean(x) 
+    sigma       = np.std(x)
+    x           = (x - mean) / sigma
+   
+    return x 
+
+    
+if __name__ == '__main__':
+    pass
+
